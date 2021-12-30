113ff22ae2231586e22791d38f4245e461de80c9
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-12-30 22:35:31 +0600

    Move state to board component

--- src/App.js
+++ src/App.js
@@ -4,47 +4,14 @@ import { Header } from './components/header/header.component';
 import { Board } from './components/board/board.component';
 import { AddScore } from './components/add-score/add-score.component';
 
-import { baseURL, gameID } from './config';
-
-import { v4 as uuidv4 } from 'uuid';
-
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      scores: [],
-      loading: true,
-    };
-  }
-
-  async componentDidMount() {
-    const response = await fetch(`${baseURL}${gameID}/scores/`);
-    const { result: scores } = await response.json();
-
-    this.setState({
-      scores: scores
-        .map((score) => ({
-          user: score.user,
-          score: score.score,
-          id: uuidv4(),
-        }))
-        .sort((score1, score2) => score2.score - score1.score),
-      loading: false,
-    });
-  }
-
   render() {
     return (
       <div className="App">
         <Header title="Leaderboard" />
         <main>
           <div className="container container--flex">
-            <Board
-              title="scores"
-              scores={this.state.scores}
-              loading={this.state.loading}
-            />
+            <Board title="scores" />
             <AddScore />
           </div>
         </main>
--- src/components/board/board.component.jsx
+++ src/components/board/board.component.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 
+import { baseURL, gameID } from '../../config';
 import { Scoreboard } from '../scoreboard/scoreboard.component';
 
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Loader from 'react-loader-spinner';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import './board.styles.css';
 
 export class Board extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      scores: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`${baseURL}${gameID}/scores/`);
+    const { result: scores } = await response.json();
+
+    this.setState({
+      scores: scores
+        .map((score) => ({
+          user: score.user,
+          score: score.score,
+          id: uuidv4(),
+        }))
+        .sort((score1, score2) => score2.score - score1.score),
+      loading: false,
+    });
+  }
+
   render() {
     return (
       <section className="board">
@@ -15,10 +43,10 @@ export class Board extends React.Component {
         <button type="button" className="btn btn-main board__btn">
           Refresh
         </button>
-        {this.props.loading ? (
+        {this.state.loading ? (
           <Loader type="TailSpin" color="#3563be" height={50} width={50} />
         ) : (
-          <Scoreboard scores={this.props.scores} />
+          <Scoreboard scores={this.state.scores} />
         )}
       </section>
     );
